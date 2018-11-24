0d0090ab291849307396ee110c5660c8835d14e3
이의섭 <beflylee@naver.com>
2018-11-24 21:54:58 +0900

    1차

--- Compass/Compass.cpp
+++ Compass/Compass.cpp
@@ -1,2 +1,56 @@
 //김경태, 이의섭, 한승남
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class User
+{
+private:
+	char password;
+	char name;
+	char sex;
+	int birth;
+	int phone;
+	bool spam;
+	char place;
+
+public:
+	User()
+	{
+		
+	}
+
+	void main()
+	{
+		char id[] = "Lee";
+		char * fileName = strcat(id, ".txt");
+		fstream fs(fileName);
+    }
+};
+/*
+class Licensee
+{
+private:
+	char password;
+	char name;
+	char agency;
+	int phone;
+	int license;
+	int grade;
+	int packgrade;
+	bool spam;
+	bool on;
+public:
+
+
+};
+class Guide
+{
+private:
+
+public:
+
+
+};
+*/
