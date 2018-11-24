06db5e6b1cb1264c6e8e5ebdafee85f2875a1e72
hansnam1105 <hansnam123@gmail.com>
2018-11-25 00:08:38 +0900

    파일입출력-1

--- Compass/Compass.cpp
+++ Compass/Compass.cpp
@@ -1,5 +1,7 @@
 //김경태, 이의섭, 한승남
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main()
@@ -11,5 +13,24 @@ class Package
 {
 protected:
 	int PID;
+	string Pname;
+	string location;
+	int price;
+	int trav_start_date;
+	int trav_start_hour;
+	int how_long_trav;
 
+public:
+	Package()
+	{
+
+	}
+	void addPackage()
+	{
+		cout << "패키지 이름: ";
+		ofstream out("packagelist.txt");
+			cin >> Pname;
+			out << Pname << endl;
+		out.close();
+	}
 };
