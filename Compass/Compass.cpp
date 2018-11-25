af43a1e094867194c687f6db5e5768a6dc52608f
이의섭 <beflylee@naver.com>
2018-11-25 21:05:49 +0900

    실험

--- Compass/Compass.cpp
+++ Compass/Compass.cpp
@@ -1,63 +1,11 @@
-//김경태, 이의섭, 한승남
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <fstream>
-#include <string>
 using namespace std;
 
 
-class User
-{
-private:
-	char password;
-	char name;
-	char sex;
-	int birth;
-	int phone;
-	bool spam;
-	char place;
-
-public:
-	User()
-	{
-		
-	}
-	void GetInfo()
-	{
-		char idOnLogin[50] = "kkt752";
-		char *fileName = strcat(idOnLogin, ".txt");
-		cout << fileName << endl;
-	}
-
-	
-};
-/*
-class Licensee
-{
-private:
-	char password;
-	char name;
-	char agency;
-	int phone;
-	int license;
-	int grade;
-	int packgrade;
-	bool spam;
-	bool on;
-public:
 
 
-};
-class Guide
-{
-private:
-
-public:
-
-
-};
-*/
 int main()
 {
-	cout << "das";
+	int n = 1;
+	cout << n << endl;
 }
