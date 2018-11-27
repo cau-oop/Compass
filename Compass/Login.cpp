b1817d03ea7d70a2f22a53b0ad218f80d0e53439
kkt752 <kkt752@cau.ac.kr>
2018-11-27 20:01:33 +0900

    Add of Login class

---
+++ Compass/Login.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstring>
+#include<string>
+#define ID_MAX_LENGTH 30
+#define PWD_MAX_LENGTH 30
+
+
+class LoginSystem {
+private:
+	static char currentLogID[ID_MAX_LENGTH + 1];
+
+public:
+
+
+};
