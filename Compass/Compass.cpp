869af6cebdaf187f728e344b7de01e787ed576f1
ROSELOVER <hansnam123@gmail.com>
2018-12-05 17:03:56 +0900

    push to master

--- Compass/Compass.cpp
+++ Compass/Compass.cpp
@@ -398,13 +398,34 @@ public:
 
 };
 
+class User
+{
+private:
+	string password;
+	string name;
+	string sex;
+	string birth;
+	string phone;
+	string spam;
+	string country;
+	string city;
+	char id[50] = "abc";
+	char pack[50] = "pack";
+	char review[50] = "review";
+	char guide[50] = "guide";
+	char rank[50] = "rank";
+	char packname[50];
+	char idtxt[50] = "abc";
+
+};
+
 class Advertise : public User, public Package
 {
 	void advertisement()
 	{
 		int ad;
 		Package pack;
-		string uname = User::getName(); //이렇게 
+		string uname = User::GetInfo(); //이렇게 
 		string country = User::getCount();
 		
 		ad = rand() % pack.v.size();
