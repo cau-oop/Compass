7b9382e3819ab0a0ebb3482d508e28f5341486b8
hansnam1105 <hansnam123@gmail.com>
2018-12-06 02:40:26 +0900

    Advertise class update

--- Compass/Advertise.h
+++ Compass/Advertise.h
@@ -22,7 +22,7 @@ class Advertise : public User, public Package
 public:
 	void advertisement_xmemeber()		//비회원일시
 	{
-		int random;
+		int randomxm; //비회원
 		string adline;
 		int numLines = 0;
 		int findline=0;
@@ -36,28 +36,40 @@ public:
 		}
 		numLines--;
 		AD.close();
-		random = rand() % numLines;
+		randomxm = rand() % numLines;
 		ifstream printAD("packagelist.txt");
 		while (getline(printAD, adline))
 		{
-			if (findline == random)
+			if (findline == randomxm)
 			{
 				cout << adline;
 			}
 			findline++;
 		}	
 	}
-	/*void advertisement_member()
+	void advertisement_member() //회원
 	{
-		string locationad;
+		string locationad[100];
+		int i = 0;
 		string mline;
 		size_t findpersonal=0;
-		User::getCountry;
+		int countnum=0;
+		int randomm; //회원
+		string country = getCountry();
 		ifstream ADMem("packagelist.txt");
 		while (!ADMem.eof())
 		{
 			getline(ADMem, mline);
-			if((findpersonal == ADMem.find( ))
+			if ((findpersonal == mline.find(country, 0)) != string::npos)
+			{
+				countnum++;
+				locationad[i] = mline;
+				i++;
+			}
+
 		}
-	}*/
+		ADMem.close();
+		randomm = rand() % countnum;
+		cout << locationad[randomm] << endl;
+	}
 };
