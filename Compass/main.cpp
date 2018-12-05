6e8317de0784866e90ce5341295c2c2437f94ee6
ROSELOVER <hansnam123@gmail.com>
2018-12-05 19:14:34 +0900

    main으로 통합

--- Compass/main.cpp
+++ Compass/main.cpp
@@ -1,7 +1,24 @@
 //김경태, 이의섭, 한승남
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include "WorkFlow_Greeter.h"
 #include "Login.h"
+#include "obectClassFile.h"
+#include "Package.h"
+#include "Advertise.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <array>
+#include <iomanip>
+#include <ctime>
+
+#define MAX_STR_LEN 4000
+
+
+using namespace std;
  
 int main()
 {
